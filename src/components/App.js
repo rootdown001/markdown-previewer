4c3be8da82beceeebe16678e1add5b0e8c6454c8
Lance Anderson <lance.a.585@gmail.com>
2023-01-16 19:44:01 -0800

    Add - Add text for editor starting value

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,11 @@ import Previewer from './Previewer';
 
 
 function App() {
-  const [editText, setEditText] = useState('## Hello World!!');
+ 
+  const initialText = 
+    '# Markdown Previewer\r\n## GitHub Flavored Markdown\r\nYou can checkout [markedjs](https://github.com/markedjs/marked) on GitHub.\r\n>⚖️ light-weight while implementing all markdown features\r\nYou can show **code blocks**\r\n\r\n```\r\nconst = example(e => (e^2))\r\n```'
+  
+  const [editText, setEditText] = useState(initialText);
 
   function handleChange(e) {
     setEditText(e.target.value);
