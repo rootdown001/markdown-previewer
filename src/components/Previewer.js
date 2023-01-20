4fd57885c04d7e226df74c003d11722c2467dbbc
Lance Anderson <lance.a.585@gmail.com>
2023-01-19 20:26:36 -0800

    Edit - Edit prism highlight for code

--- src/components/Previewer.js
+++ src/components/Previewer.js
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { marked } from 'marked';
+import Prism from 'prismjs';
+import 'prismjs/themes/prism-okaidia.css'; // import the css theme
+import 'prismjs/components/prism-javascript'; // import the javascript component
+
 
 
 
@@ -9,18 +13,26 @@ export default function Previewer({editText}) {
   const options = {
     renderer: new marked.Renderer(),
     gfm: true,
-    breaks: true
+    breaks: true,
+    highlight: function (code, lang = 'javascript') {
+      return Prism.highlight(code, Prism.languages.javascript, lang);
+    }
   };
  
 
 
+
  const html = marked(editText, options);
+
+ useEffect(() => {
+  Prism.highlightAll();
+}, [html]);
 
   return (
     <div>
       <div className="holder-previewer">
         <div className="header-box">Previewer</div>
-        <div id="preview" className="previewer__div" dangerouslySetInnerHTML={{ __html: html }} />
+        <div id="preview" className="previewer__div language-javascript" dangerouslySetInnerHTML={{ __html: html }} />
       </div>
     </div>
   )
